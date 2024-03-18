3ec768c25c56a7ceff3d77558c5451e003377156
Pascal <pascalthomasse@hotmail.com>
2024-03-18 22:32:05 +0100

    validation

--- src/Form/ForecastType.php
+++ src/Form/ForecastType.php
@@ -4,10 +4,11 @@ namespace App\Form;
 
 use App\Entity\Forecast;
 use App\Entity\Location;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ForecastType extends AbstractType
 {
@@ -24,7 +25,19 @@ class ForecastType extends AbstractType
             ->add('windSpeed')
             ->add('windDeg')
             ->add('cloudiness')
-            ->add('icon')
+            ->add('icon', ChoiceType::class, [
+                'choices' => [
+                    '(choose)' => '',
+                    'Sun' => 'sun',
+                    'Cloudy' => 'cloud',
+                    'Cloudy with Rain' => 'cloud-rain',
+                    'Cloudy with Sun' => 'cloud-sun',
+                    'Hail' => 'cloud-hail',
+                    'Snow' => 'cloud-snow',
+                    'Sleet' => 'cloud-sleet',
+                    'Thunderstorms' => 'cloud-lightning-rain',
+                ]
+            ])
             ->add('location', EntityType::class, [
                 'class' => Location::class,
                 'choice_label' => 'name',
